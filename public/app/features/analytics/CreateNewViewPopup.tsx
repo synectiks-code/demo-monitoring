9cf30e8aaf61da48f2befad84509d660fa54c626
Satish Pal <satishweb93@gmail.com>
2021-01-06 17:40:57 +0530

    Update: Create New View Popup UI in analytics page.

--- public/app/features/analytics/CreateNewViewPopup.tsx
+++ public/app/features/analytics/CreateNewViewPopup.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Modal, ModalBody } from 'reactstrap';
+import { Modal, ModalBody, ModalHeader } from 'reactstrap';
 
 export class CreateNewViewPopup extends React.Component<any, any> {
   steps: any;
@@ -24,43 +24,34 @@ export class CreateNewViewPopup extends React.Component<any, any> {
   render() {
     const { modal } = this.state;
     return (
-      <Modal isOpen={modal} toggle={this.toggle} className="modal-container">
-        <ModalBody style={{ height: 'calc(75vh - 50px)', overflowY: 'auto', overflowX: 'hidden' }}>
+      <Modal isOpen={modal} toggle={this.toggle} className="catalog-modal-container">
+        <ModalHeader toggle={this.toggle}>Creating New View</ModalHeader>
+        <ModalBody style={{ height: 'calc(45vh - 50px)', overflowY: 'auto', overflowX: 'hidden' }}>
           <div className="d-block width-100">
-            <div className="d-block p-b-20 heading">
-              <div className="d-inline-block width-75 v-a-top">
-                <h4 className="d-block">Creating New View</h4>
-              </div>
+            <div className="form-group">
+              <label htmlFor="viewName">View Name</label>
+              <input
+                type="text"
+                className="input-group-text"
+                id="viewName"
+                name="viewName"
+                placeholder="A descriptive name of View"
+              />
             </div>
-            <div className="row">
-              <div className="col-lg-12 col-md-12 col-sm-12">
-                <div className="form-group">
-                  <label htmlFor="viewName">View Name</label>
-                  <input type="text" id="viewName" name="viewName" placeholder="A descriptive name of View" />
-                </div>
-              </div>
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <textarea
+                id="description"
+                className="input-group-text"
+                name="description"
+                placeholder="What kind of Dashboards are included in this View"
+              ></textarea>
             </div>
-            <div className="row">
-              <div className="col-lg-12 col-md-12 col-sm-12">
-                <div className="form-group">
-                  <label htmlFor="description">Description</label>
-                  <textarea
-                    id="description"
-                    name="description"
-                    placeholder="What kind of Dashboards are included in this View"
-                  ></textarea>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-lg-12 col-md-12 col-sm-12">
-                <div className="d-block text-center p-t-20">
-                  <button className="cancel" onClick={this.handleClose}>
-                    Cancel
-                  </button>
-                  <button className="continue">Continue</button>
-                </div>
-              </div>
+            <div className="d-block text-right p-t-20">
+              <button className="alert-gray-button cancel" onClick={this.handleClose}>
+                Cancel
+              </button>
+              <button className="alert-blue-button m-r-0 continue">Continue</button>
             </div>
           </div>
         </ModalBody>
